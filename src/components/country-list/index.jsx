bd8f0446edae16fe3a55df3bbd363d5dc369fc2f
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-05-23 21:45:06 +0300

    Replace fetching process in coutry list component to main component

--- src/components/country-list/index.jsx
+++ src/components/country-list/index.jsx
@@ -1,17 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { List } from "./components/List";
 import { useDispatch, useSelector } from "react-redux";
 import { EmptyScreen } from "./components/EmptyScreen";
-import { fetchCountries } from "../../store/countrySlice";
 import { Loading } from "./components/Loading";
 
 function CountryList() {
 	const { filteredData, loading } = useSelector((state) => state.countries);
 	const dispatch = useDispatch();
-
-	useEffect(() => {
-		dispatch(fetchCountries());
-	}, []);
 
 	return (
 		<div className="country-list">
--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
+import { fetchCountries } from "../../store/countrySlice";
 
 function Main() {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(fetchCountries());
+	}, []);
+
 	return (
 		<main>
 			<Outlet />
