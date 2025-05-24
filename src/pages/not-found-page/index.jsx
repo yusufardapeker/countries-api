854b885e8f94db4a1532def293a4250b28b079c4
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-05-24 19:11:09 +0300

    Add not found page

--- src/pages/not-found-page/index.jsx
+++ src/pages/not-found-page/index.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NotFoundPage() {
-	return <div>NotFoundPage</div>;
+	return (
+		<div className="not-found-page">
+			<p className="not-found-message">The page you are looking for is missing...</p>
+
+			<Link to="/" className="go-home-button">
+				Go Home
+			</Link>
+		</div>
+	);
 }
 
 export { NotFoundPage };
