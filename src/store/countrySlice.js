00c466a51ac1ac5f807929df49e45f84e53aec59
Yusuf Arda Peker <yusufardapkr@gmail.com>
2025-07-24 09:37:39 +0300

    Change data source from API to local json file because the API provider

--- src/store/countrySlice.js
+++ src/store/countrySlice.js
@@ -1,8 +1,11 @@
 // features/dataSlice.js
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchCountries = createAsyncThunk("data/fetchCountries", async () => {
-	const res = await fetch("https://restcountries.com/v3.1/all");
+export const fetchCountries = createAsyncThunk("data/fetchCountries", async (data) => {
+	// This request is no longer available. More info (https://gitlab.com/restcountries/restcountries/-/issues/265)
+	// const res = await fetch("https://restcountries.com/v3.1/all");
+
+	const res = await fetch("/data.json");
 	return await res.json();
 });
 
